79af37b6219c291a662e9be77431280ffcae525e
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-04 01:12:25 +0300

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,9 @@
-
-// import {DECREASE, INCREASE} from "./actions";
+import {DECREASE, INCREASE, CLEAR_CART, GET_AMOUNT, GET_TOTAL, REMOVE} from "./actions";
 
 function reducer(state, action) {
+    if(action.type === CLEAR_CART) {
+        return {...state, cart: []}
+    }
     return state
 }
 

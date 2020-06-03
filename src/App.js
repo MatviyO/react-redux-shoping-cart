4de75baaaae73f1accbcef069a9e3ae14cd9d8e0
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-03 20:19:07 +0300

    first

--- src/App.js
+++ src/App.js
@@ -4,14 +4,45 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
-// redux stuff
+import { createStore } from "redux";
+
+const initialStore = {
+  count: 0,
+  name: 'Jon'
+};
+
+const DECREASE = "DECREASE"
+
+function reducer(state, action) {
+  console.log({ state, action });
+  if (action.type === DECREASE) {
+    return {...state, count: state.count - 1};
+  }
+  if (action.type === "INCREASE") {
+    return {...state, count: state.count + 1};
+  }
+  if (action.type === "RESET") {
+    return {...state, count: 0};
+  }
+  if (action.type === "CHANGE_NAME") {
+    return {...state, name: "new name"};
+  }
+  return state
+}
+
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
+store.dispatch({ type: "CHANGE_NAME" });
+console.log(store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );

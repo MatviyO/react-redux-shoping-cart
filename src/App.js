f973040a510b08786e393c9cb7b10bb200e17fe8
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-04 12:58:28 +0300

    payload

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     return (
         <Provider store={store}>
             <Navbar/>
-            <CartContainer cart={cartItems}/>
+            <CartContainer />
         </Provider>
     );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,19 @@
 import {DECREASE, INCREASE, CLEAR_CART, GET_AMOUNT, GET_TOTAL, REMOVE} from "./actions";
 
 function reducer(state, action) {
-    if(action.type === CLEAR_CART) {
-        return {...state, cart: []}
-    }
-    return state
+   
+   switch(action.type) {
+       case CLEAR_CART:
+           return {...state, cart: []}
+       case DECREASE:
+           return console.log()
+       case INCREASE:
+           return console.log()
+       case REMOVE:
+           return {...state, cart: state.cart.filter(item => item.id !== action.payload.id)}
+       default:
+           return state;
+   }
 }
 
 export default reducer

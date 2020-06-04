db6111cdec422a2407949366bfaed95f216ee7a5
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-04 14:04:18 +0300

    increase

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,34 @@
-import {DECREASE, INCREASE, CLEAR_CART, GET_AMOUNT, GET_TOTAL, REMOVE} from "./actions";
+import {DECREASE, INCREASE, CLEAR_CART, GET_AMOUNT, GET_TOTALS, REMOVE} from "./actions";
 
 function reducer(state, action) {
-   
+
    switch(action.type) {
        case CLEAR_CART:
            return {...state, cart: []}
        case DECREASE:
-           return console.log()
+           let temCart = []
+           if (action.payload.amount === 1) {
+               temCart =  state.cart.filter(item => item.id !== action.payload.id);
+           } else {
+                temCart = state.cart.map(item => {
+                   if (item.id === action.payload.id) {
+                       item = {...item, amount: item.amount - 1}
+                   }
+
+                   return item
+               })
+           }
+
+           return {...state, cart: temCart}
        case INCREASE:
-           return console.log()
+           let tempCart = state.cart.map(item => {
+               if (item.id === action.payload.id) {
+                   item = {...item, amount: item.amount + 1}
+               }
+
+               return item
+           })
+           return {...state, cart: tempCart}
        case REMOVE:
            return {...state, cart: state.cart.filter(item => item.id !== action.payload.id)}
        default:
